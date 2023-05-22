a38540ed5a05591382d779729b7017a1d398de48
Onwuesi Williams <onwuesiwilliamsemeka@gmail.com>
2023-05-22 13:52:20 +0100

    Sanctum Error Fixes

--- app/Http/Controllers/SPAAuthController.php
+++ app/Http/Controllers/SPAAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SPAAuthController extends Controller
 {
@@ -14,8 +15,6 @@ class SPAAuthController extends Controller
         ]);
 
         if(Auth::attempt($credentials)) {
-
-            $request->session()->regenerate();
 
             return (['message' => 'User succesfully authenticated!.']);
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 
 /**
